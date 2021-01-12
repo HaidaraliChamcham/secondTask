ccfe78b7bceb789fb7ee3d64cfae972581de5072
Raj D <rajdhami@nidhilold.local>
2021-01-12 21:10:06 +0530

    Raj-Representing data

--- frontend/src/screen/userListPage.js
+++ frontend/src/screen/userListPage.js
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const UserListPage = () => {
-    const fetchData = () =>{
-        return axios.get('https://reqres.in/api/users').then(({data}) =>{
-            console.log(data.data.map((data) =>(
-              
-                        console.log(data.email)
-                
-            )));
-            return JSON.stringify(data.data);
-        }).catch( err =>{
-            console.log(err);
-        })
-    }
+  const fetchData = () => {
+    return axios
+      .get("https://reqres.in/api/users")
+      .then(({ data }) => {
+        // console.log(data);
+        return data.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const [randomData, setRandomData] = useState('');
-    useEffect(() => {
-        fetchData().then(randomsData =>{
-            setRandomData(randomsData);
-        })
-    }, []);
-    return (
-        <div>
-            <pre>
-                {randomData}
-                
-            </pre>
-        </div>
-    );
-}
+  const [randomData, setRandomData] = useState([]);
+  useEffect(() => {
+    fetchData().then((randomsData) => {
+      setRandomData(randomsData);
+    });
+  }, []);
+  return (
+    <div>
+      <pre>
+        {randomData.map((item, index) => {
+          const { first_name, last_name, avatar, email } = item;
+          return (
+            <div key={index}>
+              <img src={avatar} />
+              <div>{first_name + " " + last_name}</div>
+              <div>{}</div>
+              <div>{email}</div>
+            </div>
+          );
+        })}
+      </pre>
+    </div>
+  );
+};
 
 export default UserListPage;
